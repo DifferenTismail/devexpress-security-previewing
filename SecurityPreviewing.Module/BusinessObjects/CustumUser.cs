21dcfddbf4de422b053541b8bc5c61f98f99b17b
İsmail Baran <ismailbaran04@gmail.com>
2024-02-27 08:49:28 +0300

    kurulum dosyaları eklendi

---
+++ SecurityPreviewing.Module/BusinessObjects/CustumUser.cs
@@ -0,0 +1,33 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using DevExpress.Persistent.BaseImpl.PermissionPolicy;
+namespace SecurityPreviewing.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    public class CustomUser : PermissionPolicyUser
+    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113146.aspx).
+        // Use CodeRush to create XPO classes and properties with a few keystrokes.
+        // https://docs.devexpress.com/CodeRushForRoslyn/118557
+        public CustomUser(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+        }
+
+    }
+}
