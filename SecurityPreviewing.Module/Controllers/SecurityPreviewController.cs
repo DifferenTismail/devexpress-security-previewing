49e3d88c78a2908303f399343f6049ab1801907e
İsmail Baran <ismailbaran04@gmail.com>
2024-02-27 11:25:12 +0300

    gerekli olan dosyalar eklendi

--- SecurityPreviewing.Module/Controllers/SecurityPreviewController.cs
+++ SecurityPreviewing.Module/Controllers/SecurityPreviewController.cs
@@ -4,10 +4,12 @@ using DevExpress.ExpressApp.Actions;
 using DevExpress.ExpressApp.Editors;
 using DevExpress.ExpressApp.Layout;
 using DevExpress.ExpressApp.Model.NodeGenerators;
+using DevExpress.ExpressApp.Security;
 using DevExpress.ExpressApp.SystemModule;
 using DevExpress.ExpressApp.Templates;
 using DevExpress.ExpressApp.Utils;
 using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl.PermissionPolicy;
 using DevExpress.Persistent.Validation;
 using System;
 using System.Collections.Generic;
@@ -19,22 +21,82 @@ namespace SecurityPreviewing.Module.Controllers
     // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppWindowControllertopic.aspx.
     public partial class SecurityPreviewController : WindowController
     {
+
         // Use CodeRush to create Controllers and Actions with a few keystrokes.
         // https://docs.devexpress.com/CodeRushForRoslyn/403133/
         public SecurityPreviewController()
         {
             InitializeComponent();
             // Target required Windows (via the TargetXXX properties) and create their Actions.
+            TargetWindowType = WindowType.Main;
         }
         protected override void OnActivated()
         {
             base.OnActivated();
             // Perform various tasks depending on the target Window.
+            LoadRoles();
         }
         protected override void OnDeactivated()
         {
             // Unsubscribe from previously subscribed events and release other references and resources.
             base.OnDeactivated();
+        }
+
+        private void LoadRoles()
+        {
+            actionSecurityPreview.Items.Clear();
+            using (IObjectSpace space = Application.CreateObjectSpace())
+            {
+                ISecurityPreview user = (ISecurityPreview)SecuritySystem.CurrentUser;
+
+                if (user.SecurityPreview)
+                    {
+                        foreach(IPermissionPolicyRole role in space.GetObjects<PermissionPolicyRole>().OrderBy(x => x.Name))
+                        {
+                              ChoiceActionItem item = new ChoiceActionItem(role.Name, role.Oid);
+                              actionSecurityPreview.Items.Add(item);
+                        }
+                    {
+                    
+                }
+                actionSecurityPreview.Items.Add(new ChoiceActionItem("Refresh...", Guid.Empty));
+                }
+            }
+        }
+        private void actionSecurityPreview_Execute(object sender, SingleChoiceActionExecuteEventArgs e)
+        {
+            Guid roleid = (Guid)e.SelectedChoiceActionItem.Data;
+
+            if (roleid.Equals(Guid.Empty))
+            {
+                LoadRoles();
+                return;
+            }
+            using (IObjectSpace space = Application.CreateObjectSpace())
+            {
+                PermissionPolicyRole role = space.GetObjectByKey<PermissionPolicyRole>(roleid);
+                PermissionPolicyUser user = space.GetObjectByKey<PermissionPolicyUser>(SecuritySystem.CurrentUserId);
+
+                if(user == null || role == null)
+                {
+                    throw new UserFriendlyException("Unable to find user or role");
+                }
+
+                while(user.Roles.Count > 0) {
+                    user.Roles.Remove(user.Roles[0]);        
+                }
+                user.Roles.Add(role);
+
+                space.CommitChanges();
+
+                Application.LogOff();
+            }
+              
+        }
+
+    public interface ISecurityPreview
+        {  
+            bool SecurityPreview { get; }
         }
     }
 }
--- SecurityPreviewing.Win/obj/Debug/net8.0-windows/SecurityPreviewing.Win.AssemblyInfo.cs
+++ SecurityPreviewing.Win/obj/Debug/net8.0-windows/SecurityPreviewing.Win.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SecurityPreviewing.Win")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21dcfddbf4de422b053541b8bc5c61f98f99b17b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62d095a8da91fd40b961e57a1fb26a0ddf7084c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SecurityPreviewing.Win")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SecurityPreviewing.Win")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.*")]
